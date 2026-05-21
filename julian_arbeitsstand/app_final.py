a421db93e03bb9e006effdf4d9001afdf457702e
Julian Leistler <julian.leistler@study.hs-duesseldorf.de>
2026-05-21 14:13:37 +0200

    fix

--- julian_arbeitsstand/app_final.py
+++ julian_arbeitsstand/app_final.py
@@ -609,7 +609,7 @@ st.sidebar.title("Magnificent 7: Risiko - Dashboard")
 start_capital = st.sidebar.number_input("Startkapital ($)", value=100_000, step=10_000)
 st.sidebar.markdown("---")
 Info_modus = st.sidebar.toggle("Info-Modus aktivieren", value=False)
-Note_Infomodus = st.info("Im Info-Modus werden zusätzliche Erklärungen und Interpretation geliefert. Nutzung nicht geeignet für stetiges an- und auschalten, da Perfomance von App darunter leiden könnte.")
+Note_Infomodus = st.sidebar.info("Im Info-Modus werden zusätzliche Erklärungen und Interpretation geliefert. Nutzung nicht geeignet für stetiges an- und auschalten, da Perfomance von App darunter leiden könnte.")
 # Tabs
 tab_uebersicht, tab_1y, tab_5y, tab_10y, tab_black_swan, tab_methoden = st.tabs([
     "Übersicht", "1-Jahres-Risiko", "5-Jahres-Risiko", "10-Jahres-Risiko", "Black-Swan-Sim","Methodenvergleich"
