b7c62e9aa2d81fbd867612d63b01523fd4dd4c53
Julian Leistler <julian.leistler@study.hs-duesseldorf.de>
2026-05-25 10:23:11 +0200

    fix

--- julian_arbeitsstand/app_final.py
+++ julian_arbeitsstand/app_final.py
@@ -510,7 +510,7 @@ def plot_density_comparison(portfolio_returns_log, portfolio_returns_discrete, s
     elif days <= 1260:
         zoom_max = start_capital * 10
     else:
-        zoom_max = start_capital * 35
+        zoom_max = start_capital * 40
 
 
     fig_pC.update_layout(
